327de93d41dd48d79ab98a92dd8c9154669062ba
Tommy <spiel.hoelle@live.de>
2018-12-18 14:38:22 +0100

    manual approach towards view engine

--- templateEngines/index.js
+++ templateEngines/index.js
@@ -1,33 +1,30 @@
-const express = require("express");
+const express = require('express')  
 const app = express();
-const Handlebars = require("handlebars");
-const path = require("path");
-
-const items = [
-  {
-    name: "Fungi",
-    price: "7",
-    onSale: true
-  },
-  {
-    name: "Hawai",
-    price: "9",
-    onSale: false
-  }
-];
-var exphbs  = require('express-handlebars');
-
-app.engine('handlebars', exphbs({layout: false}));
+const Handlebars = require('handlebars')  
+const expressHandlebars = require('express-handlebars');
+app.engine('handlebars', expressHandlebars({layout: false}));
 app.set('view engine', 'handlebars');
-
-Handlebars.registerHelper("total", function(pizzas) {
-  console.log('pizzas', pizzas);
-  let result = pizzas.reduce((acc, item) => acc + parseInt(item.price), 0);
+const items = [{
+  name: 'Fungi',
+  price: '7',
+  onSale: true
+}, {
+  name: 'Hawai',
+  price: '9',
+  onSale: false
+}]
+Handlebars.registerHelper('total', function(items) {
+  let result = items.reduce((acc, item) => acc + parseInt(item.price), 0)
   return result;
 });
-
-app.get("/", function(req, res) {
+app.get('/', function(req, res) {  
   res.render("index", {items: items});
 });
+app.get('/about', function(req, res) {  
+  res.render("about");
+});
+app.get('contact', function(req, res) {  
+  res.render("contact");
+});
 
-app.listen(8000);
+app.listen(8000);  
